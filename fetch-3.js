18b8ae94590b906dfdfb0df9429999f211fce77a
alanbacont <almabarocioco@ittepic.edu.mx>
2022-09-27 17:52:39 -0600

    Commit 3
    creacion de fetch 2 y 3

---
+++ fetch-3.js
@@ -0,0 +1,20 @@
+//Declaramos el objeto
+let usuario = {
+    nombre: 'Barocio',
+    edad: 25
+}
+
+//FIC: API POST de usuario.
+fetch('https://reqres.in/', {
+    method: 'POST',
+    body: JSON.stringify(usuario), //data a enviar
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(resp => resp.json())
+.then(console.log)
+.catch(error => {
+    console.log('Error en la peticion API POST');
+    console.log(error);
+});
