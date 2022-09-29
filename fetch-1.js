7700706e28188c1d6a2d5197a1dd34ca29c5ff69
alanbacont <almabarocioco@ittepic.edu.mx>
2022-09-29 07:06:53 -0600

    Update fetch-1.js

--- fetch-1.js
+++ fetch-1.js
@@ -4,15 +4,15 @@ request.open('GET', 'https://reqres.in/api/users', true );
 request.send(null);
 request.onreadystatechange = function( state ){
     if (request.readyState === 4) {
-        //FIC: la respuesta obtiene un string.
+        //La respuesta obtiene un string.
         var resp = request.response;
-        //FIC: ahora transformamos la cadena en formato
+        //transformamos la cadena en formato
         var respObj = JSON.parse(resp);
-        //FIC: enviamos el resultado a la consola.
+        //Enviamos el resultado a la consola.
         console.log( respObj );
-        //FIC: obtenemos solo la pagina actual.
+        //Obtenemos solo la pagina actual.
         console.log( respObj.page);
-        //FIC: obtenemos solo la data de los usuarios.
+        //Obtenemos solo la data de los usuarios.
         console.log( respObj.data);
     }   
 }
